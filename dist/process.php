4c488ee794835015953037617010e939ab6249c1
HackerGal <mmolifie@gmail.com>
2021-02-21 18:21:01 +0200

    success feature added

--- dist/process.php
+++ dist/process.php
@@ -3,15 +3,17 @@ if (isset($_POST['email'])) {
 
 // Email info
   $admin_email = "admin@melzy.co.za";
- $name = $_POST['name'];
+  //$admin_email = "mmolifie@gmail.com";
+  $name = $_POST['name'];
   $email = $_POST['email'];
   $phone = $_POST['phone'];
-  $message = $_POST['message'];
   $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
 // To send the email
   mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
   header('Location: http://melzy.co.za/success.html');
 //  echo "Thank you $_GET["name"]";
-  echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
- }?>
+//  echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+ }
+
